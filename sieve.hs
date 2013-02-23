04c706237d8ae072fe0f81369d3cdfb6f7da53b2
Bart Massey <bart@cs.pdx.edu>
2013-02-22 23:13:39 -0800

    implemented "limit" version of sieve for performance

--- sieve.hs
+++ sieve.hs
@@ -20,8 +20,29 @@ soe n filters =
     _ -> 
       n : soe (n + 2) ([3 * n, 5 * n ..] `insert` filters)
   
+soeLim :: Integer -> Integer -> Set [Integer] -> [Integer]
+soeLim n lim _ | n > lim =
+  []
+soeLim n lim filters =
+  case findMin filters of
+    (m : _) 
+      | m < n -> error "internal error: unadvanced filter"
+      | m == n -> soeLim (n + 2) lim (advance n filters)
+    _ -> 
+      n : soeLim (n + 2) lim filters'
+  where
+    filters' =
+      if n * n <= lim
+      then [3 * n, 5 * n ..] `insert` filters
+      else filters
+  
 primes :: [Integer]
 primes = 2 : 3 : soe 5 (singleton [9, 15 ..])
 
+primesLim :: Integer -> [Integer]
+primesLim n = 2 : 3 : soeLim 5 n (singleton [9, 15 ..])
+
 main :: IO ()
-main = print $ last $ takeWhile (< 2000000) primes
+main = do
+  print $ length $ takeWhile (<2000000) $ primes
+  print $ length $ primesLim 1999999
