d37588143993dbab604f19b3d5d8538c2014f1ed
Bart Massey <bart@cs.pdx.edu>
2013-02-22 23:04:48 -0800

    split out advance

--- sieve.hs
+++ sieve.hs
@@ -3,21 +3,22 @@
 
 import Data.Set
 
+advance :: Integer -> Set [Integer] -> Set [Integer]
+advance n fs =
+  case deleteFindMin fs of
+    (m : ms, fs') 
+      | m <= n -> advance n (ms `insert` fs')
+      | m > n -> fs
+    _ -> error "internal error: bad advance"
+
 soe :: Integer -> Set [Integer] -> [Integer]
 soe n filters =
   case findMin filters of
     (m : _) 
       | m < n -> error "internal error: unadvanced filter"
-      | m == n -> soe (n + 2) (advance filters)
+      | m == n -> soe (n + 2) (advance n filters)
     _ -> 
       n : soe (n + 2) ([3 * n, 5 * n ..] `insert` filters)
-  where
-    advance fs =
-      case deleteFindMin fs of
-        (m : ms, fs') 
-          | m <= n -> advance (ms `insert` fs')
-          | m > n -> fs
-        _ -> error "internal error: bad advance"
   
 primes :: [Integer]
 primes = 2 : 3 : soe 5 (singleton [9, 15 ..])
